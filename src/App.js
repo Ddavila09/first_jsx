e09fe3251e40c24243f9460613bbb029b4e58b67
Ddavila09 <ddavilax23@gmail.com>
2023-05-04 12:55:04 -0500

    first_react

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="container-header">
+        <h1> Hello Dojo</h1>
+        <ul> Things I need to do today: 
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+
       </header>
     </div>
   );
